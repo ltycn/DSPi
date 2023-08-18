c644e53443439b94ff3304cb257afb8d7aa587db
劉天義 <i@terry.ee>
2023-08-18 19:27:52 +0800

    update

---
+++ src/token.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSPi
+{
+    public class token
+    {
+        public const string accesstoken = "29faf69f3298f0f95d31d3c9820bd135dfa06641";
+    }
+}
